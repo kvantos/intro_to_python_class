30773bb47588f53e939cc39941e0d062a9498393
Sergey Kviato <skviato@slice.com>
2017-07-13 16:07:42 +0300

    Submit 2 assignment. Exercises 7,8

--- A2.py
+++ A2.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env python3
 
+"""
+7. Преобразовать строку с американским форматом даты в европейский.
+Например, "05.17.2016" преобразуется в "17.05.2016"
 
+8. Написать программу, которая берет две строки и помещает первую
+строку в середину второй. Результат помещается в середину первой.
+Длину строки можно получить с помощью функции len().
+Для простоты можно считать, что длины строк четные.
+"""
+
+# ex7 method 1
+us_date = "01.20.1970"
+
+month = us_date[:2]
+day = us_date[3:5]
+year = us_date[-4:]
+
+eu_date = day + "." + month + "." + year
+
+print("Date in US format %s transformed in to EU format %s"
+      % (us_date, eu_date))
+
+# ex7 method 2
+us_date = "02.21.1970"
+
+(month, day, year) = us_date.split('.')
+eu_date = ("%s.%s.%s" % (day, month, year))
+
+print("Date in US format %s transformed in to EU format %s"
+      % (us_date, eu_date))
+
+# ex8
+dave = "I shall be forced to disconnect you"
+hal9000 = "I was only trying to do what I thought best."
+
+center_of_hal9000 = len(hal9000)//2
+step1 = hal9000[:center_of_hal9000] + dave + hal9000[center_of_hal9000:]
+print(step1)
+
+center_of_dave = len(dave)//2
+step2 = dave[:center_of_dave] + step1 + dave[center_of_dave:]
+print(step2)
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# intro_to_python_class
+# Intro to Python class
 Purpose of this project is to share HW with teacher at Hillel IT School
 
-# A1.py
+### A1.py
 First hw. Is about to complete exercises from 1 to 6.
+
+### A2.py
+Seccond assignment. Exercises 7,8
